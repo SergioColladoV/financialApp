b276482e9e378cc8fde05fea472073236d02d008
Sergio Collado <sergiocollado95@gmail.com>
2020-05-09 19:03:55 +0200

    Client cleaned and Styled Components installed

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,11 @@
+// IMPORT REACT
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+// IMPORT COMPONENTS
 import App from './App';
+
+// IMPORT SERVICEWORKERS
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
